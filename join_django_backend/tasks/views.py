5027e6975f7bc355d7dde61df8dbc9555e68d35a
Matthias Plank <matthias.plank@gmx.at>
2024-01-04 21:54:20 +0100

    create view task

--- join_django_backend/tasks/views.py
+++ join_django_backend/tasks/views.py
@@ -1,9 +1,11 @@
+import json
 from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework import viewsets
 from django.core import serializers
 from .serializers import TaskSerializer
 from tasks.models import Task
+from datetime import datetime
 
 # Create your views here.
 class TaskViewSet(viewsets.ModelViewSet):
@@ -32,4 +34,31 @@ class TaskViewSet(viewsets.ModelViewSet):
         #return HttpResponse(serialized_obj, content_type='application/json')
         #return self.queryset
         return HttpResponse(self.queryset, content_type='application/json')
-'''
+    '''
+    
+    def create(self, request):
+        print(request.body)
+        newTask = json.loads(request.body)
+        print(newTask)
+        
+        date_str = newTask['dueDate']
+        date_format = '%Y-%m-%d'
+
+        date_obj = datetime.strptime(date_str, date_format)
+        print(date_obj)
+
+
+        task = Task.objects.create(title = newTask['title'], 
+                                    description = newTask['description'], 
+                                    category = newTask['category'],
+                                    created_at = date_obj, 
+                                    assigned = newTask['assigned'],
+                                    kanban = newTask['kanban'], 
+                                    priority = newTask['priority'], 
+                                    subtasks = newTask['subtasks'], 
+                                    subtasksstatus = newTask['subtaskStatus'],
+                                    ) 
+        
+        serialized_obj = serializers.serialize('json', [task, ]) 
+    
+        return HttpResponse(serialized_obj, content_type='application/json')
