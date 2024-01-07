831a8c94d88c7574654c3d313170b6d0bd4ebaf6
Matthias Plank <matthias.plank@gmx.at>
2024-01-07 22:15:40 +0100

    task serializer

--- join_django_backend/tasks/serializers.py
+++ join_django_backend/tasks/serializers.py
@@ -1,8 +1,18 @@
+import ast
+import json
 from rest_framework import serializers
 
 from .models import Task
-
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = Task
-        fields = '__all__' 
+        fields = '__all__'
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+       # representation['assigned'] = representation['assigned'].replace("'", "\"")
+        print( representation['assigned'])
+        #representation['assigned'] = ast.literal_eval( representation['assigned'])
+
+        return representation
