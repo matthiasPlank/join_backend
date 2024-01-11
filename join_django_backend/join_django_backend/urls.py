7c1411a6b93239897797f97331eba9992c6e3ae8
Matthias Plank <matthias.plank@gmx.at>
2024-01-11 21:41:43 +0100

    auth-view

--- join_django_backend/join_django_backend/urls.py
+++ join_django_backend/join_django_backend/urls.py
@@ -14,19 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from tasks import views
+from login.views import CustomAuthToken
+from tasks import views as tasks_views
+from tasks import views as login_views
+
 from django.contrib import admin
 from django.urls import include, path
 
 from rest_framework import routers
 
-
+from rest_framework.authtoken import views as auth_views
 
 router = routers.DefaultRouter()
-router.register(r'tasks', views.TaskViewSet)
-router.register(r'contacts', views.ContactViewSet)
+router.register(r'tasks', tasks_views.TaskViewSet)
+router.register(r'contacts', tasks_views.ContactViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-token-auth/', CustomAuthToken.as_view()),
 ]
--- join_django_backend/login/views.py
+++ join_django_backend/login/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated 
+
+# Create your views herefrom rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+from django.contrib.auth.models import User
+
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+
+        print(request.data['email'])
+        #username = User.objects.get(email=request.data['email'])
+        #print(username)
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
