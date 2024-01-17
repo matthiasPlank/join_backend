23a137daca7203b16a2d40151a98b083a0f6a334
Matthias Plank <matthias.plank@gmx.at>
2024-01-17 20:21:05 +0100

    add static

--- join_django_backend/join_django_backend/settings.py
+++ join_django_backend/join_django_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-dyfa9q=j^tpa%tnrlyk!q35(2!g8l)r3pljp-&-kwax(l-lf7n
 DEBUG = True
 
 ALLOWED_HOSTS = [
-        "https://matthiasplank.pythonanywhere.com/"
+        "matthiasplank.pythonanywhere.com"
 ]
 
 
@@ -126,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -156,4 +158,5 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:4200", 
     "http://localhost:8080", 
     "http://127.0.0.1:5500",
-    "https://matthiasplank.pythonanywhere.com/"]
+    "https://matthiasplank.pythonanywhere.com/"
+    ]
